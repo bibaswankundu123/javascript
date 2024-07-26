13d9f5638a80e61202f2660abbaa1f47734f5532
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-07-26 21:02:05 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 233
+let accountEmail = "bibaswan@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+accountEmail = "bk.com"
+accountPassword = "1234"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" ; //treat all js as newer version
+
+// alert(3+3) // we are using nodes js, not browser
+
+
+let name = "Bibaswan"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//Primitive data types
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
