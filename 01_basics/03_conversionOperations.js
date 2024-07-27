64f28025ec60987a7b4d469ee3d1c21023cc4d41
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-07-27 12:49:21 +0000

    container config done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,7 +1,7 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 //"33" => 33
 // "33abc" => NaN
@@ -9,4 +9,16 @@ console.log(typeof (score));
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ***************************************************Operations***************************************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = "Bibaswan"
+
+let str3 = str1+str2
+// console.log(str3);
+
