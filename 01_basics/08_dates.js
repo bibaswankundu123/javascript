b149fe5ff5076405076be3a6e5b52bdab8cf5675
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-07-28 20:43:20 +0000

    config done

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,17 @@
+//Dates
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2024, 0, 28,5,3)
+let myCreateDate = new Date("2024-07-28")
+// console.log(myCreateDate.toLocaleString());
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime())
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
