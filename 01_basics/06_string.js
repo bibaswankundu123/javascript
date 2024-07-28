249bdec8e5bfbad5e3d6fd65f3227783f566c9ac
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-07-28 19:14:15 +0000

    config done

---
+++ 01_basics/06_string.js
@@ -0,0 +1,27 @@
+const name ="Bibaswan"
+const repoCount =5
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+const gameName = new String('Pes-konami-football')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf('e'));
+//  const newString = gameName.substring(0,4)
+//  console.log(newString);
+
+//  const anotherString = gameName.slice(-1,4)
+//  console.log(anotherString);
+
+//  const newString = " Bibaswan "
+//  console.log(newString);
+//  console.log(newString.trim());
+
+//  const url = "https://bibaswan.com/bibaswan%20kundu"
+
+//  console.log(url.replace('%20','-')); 
+//  console.log(url.includes('su'));
+
+console.log(gameName.split('-'));
