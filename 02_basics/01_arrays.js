c021390a06f5b38bff14958d99ffd2b29a7a62e8
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-08-01 12:54:11 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,23 @@
+const myArr = [0,1,5,3,4,5]
+const myHero = ["Spiderman","Batman","Joker"]
+const myArr2 = new Array(1,2,3,4,5)
+
+// myArr.push(8)
+// myArr.pop(8)
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(3));
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log( typeof newArr);
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
+
